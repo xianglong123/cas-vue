16baf6084722b14a798f7907583f4c8a60e5b738
xianglong <1391086179>
2021-01-10 21:31:06 +0800

    + echarts

--- src/main.js
+++ src/main.js
@@ -4,14 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// import ECharts from 'vue-echarts/components/ECharts.vue'
-
-// 手动引入 ECharts 各模块来减小打包体积
-// import 'echarts/lib/chart/bar'
-// import 'echarts/lib/component/tooltip'
-
 Vue.config.productionTip = false
-// Vue.use(ECharts)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Echart from '@/components/Echart/Echart'
+import Ec from '@/components/Ec'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path: '/', name: 'HelloWorld', component: HelloWorld},
-    {path: '/echart', name: 'echart', component: Echart}
+    {path: '/', name: 'echart', component: Ec}
   ]
 })
